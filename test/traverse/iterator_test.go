c796b2f8a9be6f7debe0af01f582bf76a94e699b
mojo <excessivespeed@126.com>
2017-09-13 20:50:09 +0800

    add contains test

--- test/traverse/iterator_test.go
+++ test/traverse/iterator_test.go
@@ -2,6 +2,9 @@ package traverse
 
 import (
 	"testing"
+	"wise-catalog/helper"
+
+	"fmt"
 
 	"github.com/mojo-zd/go-library/debug"
 	"github.com/mojo-zd/go-library/traverse"
@@ -9,11 +12,20 @@ import (
 
 type Person struct {
 	Name string
+	Sex  int
 }
 
 func Test_Iterator_(t *testing.T) {
-	persons := []Person{Person{Name: "mojo"}}
+	persons := []Person{{Name: "mojo"}}
 	traverse.Iterator(persons, func(index int, value interface{}) {
 		debug.Display("", value)
 	})
+}
+
+func Test_Contains(t *testing.T) {
+	person1 := Person{Name: "mojo", Sex: 5}
+	person2 := Person{Name: "mt", Sex: 4}
+	person3 := Person{Name: "mojo", Sex: 5}
+	persons := []Person{person1, person2}
+	fmt.Println(helper.Contains(persons, person3))
 }
