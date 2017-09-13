ebd72497231fd3df2c744d9e6ebf3ddbe3fc6e1b
mojo <excessivespeed@126.com>
2017-09-13 20:47:06 +0800

    add contains func

--- traverse/iterator.go
+++ traverse/iterator.go
@@ -6,7 +6,7 @@ type IteratorFunc func(index int, value interface{})
 
 func Iterator(collection interface{}, handlerFunc IteratorFunc) {
 	v := reflect.ValueOf(collection)
-	if v.Kind() != reflect.Slice {
+	if !isSlice(collection) {
 		panic("collection must be slice!")
 		return
 	}
@@ -14,4 +14,30 @@ func Iterator(collection interface{}, handlerFunc IteratorFunc) {
 	for index := 0; index < v.Len(); index++ {
 		handlerFunc(index, v.Index(index).Interface())
 	}
+}
+
+func Contains(collection interface{}, target interface{}) (contains bool) {
+
+	if !isSlice(collection) {
+		panic("collection must be slice!")
+		return
+	}
+	v := reflect.ValueOf(collection)
+	for index := 0; index < v.Len(); index++ {
+		if v.Index(index).Interface() == target {
+			contains = true
+			break
+		}
+	}
+	return
+}
+
+func isSlice(value interface{}) (slice bool) {
+	v := reflect.ValueOf(value)
+	slice = true
+	if v.Kind() != reflect.Slice {
+		slice = false
+		return
+	}
+	return
 }
